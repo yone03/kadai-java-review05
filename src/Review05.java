8887144a88483be167b803448f4b6d25ed46cea3
yone03 <starmimi29@yahoo.co.jp>
2024-04-02 17:24:37 +0900

    lesson9 課題コミット

---
+++ src/Review05.java
@@ -0,0 +1,99 @@
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class Review05 {
+
+    public static void main(String[] args) {
+        Connection con = null;
+        PreparedStatement pstmt = null;
+        ResultSet rs = null;
+
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            con = DriverManager.getConnection(
+                    "jdbc:mysql://localhost/kadaidb?useSSL=false&allowPublicKeyRetrieval=true",
+                    "root",
+                    "2024mi0401sor"
+                );
+
+            String sql = "SELECT * FROM person WHERE id = ?";
+            pstmt = con.prepareStatement(sql);
+
+            System.out.print("検索キーワードを入力してください > ");
+            int input = keyInNum();
+
+            // PreparedStatementオブジェクトの?に値をセット
+            pstmt.setInt(1, input);
+            rs = pstmt.executeQuery();
+
+            // 結果を表示
+            while ( rs.next()) {
+                // Name列の値を取得
+                String name = rs.getString("Name");
+                // age列の値を取得
+                int age = rs.getInt("age");
+                // 取得した値を表示
+                System.out.println(name);
+                System.out.println(age);
+            }
+
+        } catch (ClassNotFoundException e) {
+            System.err.println("JDBCドライバのロードに失敗しました。");
+            e.printStackTrace();
+        } catch (SQLException e) {
+            System.err.println("データベースに異常が発生しました。");
+            e.printStackTrace();
+        }finally {
+            if( rs != null ){
+                try {
+                    rs.close();
+                } catch (SQLException e) {
+                    System.err.println("ResultSetを閉じるときにエラーが発生しました。");
+                    e.printStackTrace();
+                }
+            }
+            if( pstmt != null ){
+                try {
+                    pstmt.close();
+                } catch (SQLException e) {
+                    System.err.println("Statementを閉じるときにエラーが発生しました。");
+                    e.printStackTrace();
+                }
+            }
+            if(con != null) {
+                try {
+                    con.close();
+                } catch (SQLException e) {
+                    System.err.println("データベース切断時にエラーが発生しました。");
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    private static String keyIn() {
+        String line = null;
+        try {
+            BufferedReader key = new BufferedReader(new InputStreamReader(System.in));
+            line = key.readLine();
+        } catch (IOException e) {
+        }
+        return line;
+    }
+    private static int keyInNum() {
+        int result = 0;
+        try {
+            result = Integer.parseInt(keyIn());
+        } catch (NumberFormatException e) {
+        }
+        return result;
+    }
+
+}
